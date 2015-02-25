088ecf0c4a1ad021842a820fb4e0ff83159c4e3d
jmarsh410 <jmarsh410@gmail.com>
2015-02-24 19:02:48 -0600

    updated object structure
    -combined function for adding meal and item
    -created helper function for creating a list an array of items that are NOT already on the items list

--- js/grocery.js
+++ js/grocery.js
@@ -3,15 +3,8 @@
 
 	var items = [
 		{
-			name: 'peaches'
-		},
-		{
-			name: 'steak'
-		},
-		{
-			name: 'asparagus'
-		},
-
+			name: 'peaches',
+		}
 	];
 
 	var meals = [
@@ -44,58 +37,130 @@
 			],
 		},
 	];
+
+	if (localStorage && localStorage.getItem('items')){
+		//if items exists use items from local storage
+	} else {
+		// if items doesn't exist, create it
+		localStorage.setItem('items', JSON.stringify(items));
+		var localItems = JSON.parse(localStorage.getItem('items'));
+		console.log(localItems);
+	}
+
+	
+
 	
 	app.controller('GroceryForm', function(){
-		this.item = {};
-		this.addItem = function(){
-			items.push(this.item);
-			this.item = {}; //resets item 
+		
+		// our item object
+		this.item = {
+			// type: "",
+			// name: "",
+			// ingredients: [],
+			// onList: "",
 		};
 
-		this.tab = 1;
+		// takes care of tabs AND item type
+		// AND creating item.ingredients array
+		this.tab = 1; //initializes tab #
+		this.item.type = 'single'; //initializes item type
 		this.setTab = function(tabNumber){
 			this.tab = tabNumber;
+			if (this.tab === 1) {
+				this.item.type = 'single';
+			} else {
+				this.item.type = 'meal';
+				this.item.ingredients = [];
+			}
 		};
+		// used for adding active class to nav tabs
 		this.isSelected = function(checkTab){
 			return this.tab === checkTab;
 		};
 
 		this.meals = meals;
-
-		// meal-maker functions
-		this.meal = {
-			ingredients: [],
-		};
-		this.addMeal = function(){
-			// push onto meals array, then push its ingredients onto the items array
-			meals.push(this.meal);
-			this.meal.ingredients.forEach(function(ingredient){
-				items.push(ingredient);
+		this.addItem = function(){
+			// array of available items
+			var notOnList = this.checkList(this.item);
+			// adds items to list
+			notOnList.forEach(function(listItem){
+				items.push(listItem);
+				console.log(localItems);
 			});
-			this.meal = {
-				ingredients: [],
-			};
+			if (this.item.type === 'single'){
+				//resets item object
+				this.item = {
+					type: 'single',
+				};
+			} else if (this.item.type === 'meal'){ //otherwise, its a meal
+				// push onto meals array, then push its ingredients onto the items array
+				meals.push(this.item);
+
+				// resets item object
+				this.item = {
+					type: 'meal',
+					ingredients: [],
+				};
+			} else {
+				throw new Error("item.type is " + this.item.type);
+			}
 		};
 
 		this.addMealToList = function(meal){
-			meal.ingredients.forEach(function(ingredient){
-				items.push(ingredient);
+			// array of all object's names in items array
+			var itemNames = items.map(function(item){
+				return item.name;
 			});
-			return meal;
+			// goes over every ingredient, if it isn't already in items, then it is added
+			meal.ingredients.forEach(function(ingredient){
+				if (itemNames.indexOf(ingredient.name) === -1) {
+					items.push(ingredient);
+				}
+			});
+			// return meal;
 		};
 
-		//THOUGHT - combine function for adding item and meal
-		// include if statement for distinguishing between them
+		//returns array of ingredients that aren't on the list
+		this.checkList = function(itemOrIngredient){
+			// new array of all object's names in items array
+			var itemNames = items.map(function(item){
+				return item.name;
+			});
+
+			if (this.item.type === 'meal') { //if it has ingredients, its a meal
+				// returns new array of ingredients NOT on items array
+				var notOnList = itemOrIngredient.ingredients.map(function(ingredient){
+					if (itemNames.indexOf(ingredient.name) === -1) {
+						return ingredient;
+					}
+				});
+				return notOnList;
+			} else { // otherwise, its a single item
+				if (itemNames.indexOf(itemOrIngredient.name) === -1) {
+					return new Array(itemOrIngredient);
+				} else {
+					return [];
+				}
+			} 
+		};
+
+		// disables "add meal" button if meal lacks ingredients
+		this.isDisabled = function(){
+			// first, ingredients must exist in the object, then it must equal 0
+			return this.item.ingredients && this.item.ingredients.length === 0;
+		};
 
 		// ingredient functions for a meal
-		this.ingredient = {};
+		this.ingredient = {
+			// name: "",
+		};
 		this.addIngr = function(){
-			this.meal.ingredients.push(this.ingredient);
+			this.item.ingredients.push(this.ingredient);
 			this.ingredient = {};
 		};
 		this.removeIngr = function(ingredient){
-			var ingrIndex = this.meal.ingredients.indexOf(ingredient);
-			this.meal.ingredients.splice(ingrIndex, 1);
+			var ingrIndex = this.item.ingredients.indexOf(ingredient);
+			this.item.ingredients.splice(ingrIndex, 1);
 		};
 	});
 
